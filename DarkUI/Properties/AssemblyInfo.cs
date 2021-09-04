3bbecd3158ad36f636a03de86dc1e25fd9d7892b
Micheal colhoun <micheal@cts-ni.com>
2021-09-04 12:28:24 +0100

    Update Package info to DarkUI.Net5 1.0.0  for publish to NuGet

--- DarkUI/Properties/AssemblyInfo.cs
+++ DarkUI/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DarkUI")]
-[assembly: AssemblyDescription("Dark themed control and docking library for .NET WinForms.")]
+[assembly: AssemblyDescription("Dark themed control and docking library for .NET WinForms. Based on the original DarkUI package by Robin Perris")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robin Perris")]
-[assembly: AssemblyProduct("DarkUI")]
+[assembly: AssemblyProduct("DarkUI.Net5")]
 [assembly: AssemblyCopyright("Copyright © Robin Perris")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
