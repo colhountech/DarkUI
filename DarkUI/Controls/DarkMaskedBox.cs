69104dbe508c36aa7ad1c42f2e87a5b822f3b769
Ömer Ferhat Şenel <62851645+grousa@users.noreply.github.com>
2023-01-04 17:10:18 +0300

    Added DarkMaskedBox
    Added DarkMaskedBox.

---
+++ DarkUI/Controls/DarkMaskedBox.cs
@@ -0,0 +1,20 @@
+﻿using DarkUI.Config;
+using System.Windows.Forms;
+
+namespace DarkUI.Controls
+{
+    public partial class DarkMaskedBox : MaskedTextBox
+    {
+        #region Constructor Region
+
+        public DarkMaskedBox()
+        {
+            BackColor = Colors.LightBackground;
+            ForeColor = Colors.LightText;
+            Padding = new Padding(2, 2, 2, 2);
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        #endregion
+    }
+}
